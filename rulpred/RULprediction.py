bfb96d2dde227b54107fd2deee3faecd52afe01c
Atlasov Alexander <sasha.atlasov@mail.ru>
2024-06-03 18:20:10 +0300

    minor fix

--- rulpred/RULprediction.py
+++ rulpred/RULprediction.py
@@ -27,10 +27,9 @@ class RULpredictor(nn.Module):
         super().__init__()
         self.rul_max = rul_max
 
-        self.transformer = DAST(time_step = window_size + 2, 
-                                input_size, dec_seq_len,
+        self.transformer = DAST(input_size, dec_seq_len,
                                 dim_val_s, dim_attn_s, dim_val_t, dim_attn_t, dim_val, dim_attn, 
-                                n_decoder_layers, n_encoder_layers, n_heads, dropout)
+                                n_decoder_layers, n_encoder_layers, n_heads, dropout, time_step = window_size + 2)
         
         self.eps_theta = ConditionalGuidedModel(num_timesteps=num_timesteps, 
                                                 data_dim=self.transformer.dec_seq_len * self.transformer.dim_val)
